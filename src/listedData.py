4431d9a103cdba0adbeb94334e5614a786dd8b5b
Vinícius <viniciussmmedeiros@gmail.com>
2022-04-29 16:36:54 -0300

    adicionando conversões de status para dicionarios, comentados para possivel uso futuro

--- src/listedData.py
+++ src/listedData.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
 data = pd.read_csv("./src/data.csv")
+# dataDF = pd.DataFrame(data, columns=['PLAYER', 'CLUB', 'POS', 'OVR', 'PAC', 'SHO', 'PAS', 'DRI', 'DEF', 'PHY'])
+# listedData = [i[0] for i in dataDF.values.tolist()]
+
 
 playerNamesData = pd.DataFrame(data, columns=['PLAYER'])
 playerNames = [i[0] for i in playerNamesData.values.tolist()]
+# playerNamesDict = playerNamesData.to_dict()
+# print(playerNamesDict)
 
 clubsData = pd.DataFrame(data, columns=['CLUB'])
 clubs = [i[0] for i in clubsData.values.tolist()]
@@ -13,24 +18,31 @@ position = [i[0] for i in positionData.values.tolist()]
 
 overallData = pd.DataFrame(data, columns=['OVR'])
 overall = [i[0] for i in overallData.values.tolist()]
+# overallDict = overallData.to_dict()
 
 paceData = pd.DataFrame(data, columns=['PAC'])
 pace = [i[0] for i in paceData.values.tolist()]
+# paceDict = paceData.to_dict()
 
 shootingData = pd.DataFrame(data, columns=['SHO'])
 shooting = [i[0] for i in shootingData.values.tolist()]
+# shootingDict = shootingData.to_dict()
 
 passingData = pd.DataFrame(data, columns=['PAS'])
 passing = [i[0] for i in passingData.values.tolist()]
+# passingDict = passingData.to_dict()
 
 dribblingData = pd.DataFrame(data, columns=['DRI'])
 dribbling = [i[0] for i in dribblingData.values.tolist()]
+# dribblingDict = dribblingData.to_dict()
 
 defendingData = pd.DataFrame(data, columns=['DEF'])
 defending = [i[0] for i in defendingData.values.tolist()]
+# defendingDict = defendingData.to_dict()
 
 physicalData = pd.DataFrame(data, columns=['PHY'])
 physical = [i[0] for i in physicalData.values.tolist()]
+# physicalDict = physicalData.to_dict()
 
 #################### IMPRIMINDO PARA VERIFICAR SE OS DADOS FORAM CAPTURADOS CORRETAMENTE DO ARQUIVO PARA AS LISTAS ####################
 # dataArr = []
@@ -48,3 +60,5 @@ physical = [i[0] for i in physicalData.values.tolist()]
 # for data in dataArr:
 #     print("\n\n", data)
 #######################################################################################################################################
+
+# print(listedData)
